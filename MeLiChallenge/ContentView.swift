e958944025baaf2151be1477c2f68e044048f610
Marian <hmarianj25@gmail.com>
2025-06-25 14:42:51 -0300

    Create Xcode Project

---
+++ MeLiChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeLiChallenge
+//
+//  Created by MH on 25/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeLiChallenge/MeLiChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeLiChallengeApp.swift
+//  MeLiChallenge
+//
+//  Created by MH on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MeLiChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
